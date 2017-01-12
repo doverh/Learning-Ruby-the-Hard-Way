4342b8fb4554ae4cf660abedbb9f2d02fd755824
doverh <doverheld@gmail.com>
2017-01-12 13:19:41 -0500

    first commit

---
+++ exercises/ex3.rb
@@ -0,0 +1,34 @@
+#Thats just a comment
+puts "I will now count my chickens"
+
+#Mathematical operation. / should be calculated first
+puts "Hens #{25 + 30 / 6}"
+
+#Another Math operation
+puts "Roosters #{100 - 25 * 3 % 4}"
+
+#Just another comment
+puts "Now I will count the eggs:"
+
+#Math operation should respect order  / , %, + , -
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+#Just a comment, should not result any calculation
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+#Math operation should respect order   + , -, <
+puts 3 + 2 < 5 - 7
+
+#Thats a comment with a math operation
+puts "Whats is 3 + 2? #{3 + 2}"
+
+#Thats a comment with a math operation
+puts "Whats is 5 - 7? #{5 - 7}"
+
+puts "Oh, that's why it's false."
+
+puts "How about some more."
+
+puts "Is it greater? #{5 > -2}"
+puts "Is it greater or equal? #{5 >= -2}"
+puts "Is it less or equal? #{5 <= -2}"
