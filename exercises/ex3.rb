929799cc1fdf8a5cfa569b827ed56d8e43ff172e
doverh <doverheld@gmail.com>
2017-01-12 13:35:28 -0500

    Changes on exercise 3

--- exercises/ex3.rb
+++ exercises/ex3.rb
@@ -2,7 +2,7 @@
 puts "I will now count my chickens"
 
 #Mathematical operation. / should be calculated first
-puts "Hens #{25 + 30 / 6}"
+puts "Hens #{25.0 + 30.0 / 6.0}"
 
 #Another Math operation
 puts "Roosters #{100 - 25 * 3 % 4}"
@@ -11,7 +11,7 @@ puts "Roosters #{100 - 25 * 3 % 4}"
 puts "Now I will count the eggs:"
 
 #Math operation should respect order  / , %, + , -
-puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
 
 #Just a comment, should not result any calculation
 puts "Is it true that 3 + 2 < 5 - 7?"
@@ -25,10 +25,17 @@ puts "Whats is 3 + 2? #{3 + 2}"
 #Thats a comment with a math operation
 puts "Whats is 5 - 7? #{5 - 7}"
 
+#Just a comment
 puts "Oh, that's why it's false."
 
+#Just a comment
 puts "How about some more."
 
+#Checking if 5 is greater than -2
 puts "Is it greater? #{5 > -2}"
+
+#Checking if 5 is greater or equal to -2
 puts "Is it greater or equal? #{5 >= -2}"
+
+#Checking if 5 is less or equal to -2
 puts "Is it less or equal? #{5 <= -2}"
