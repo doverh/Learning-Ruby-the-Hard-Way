989650c9a478461b250779ba0c739eac71f56edc
doverh <doverheld@gmail.com>
2017-01-17 15:24:48 -0500

    Adding exercise 5

--- exercises/ex5.rb
+++ exercises/ex5.rb
@@ -1,18 +1,21 @@
-my_name = 'Dover'
-my_age = 32 #in january 2017
-my_height = 180 #centimeters
-my_weight = 72 #kg
-my_eyes = 'Blue'
-my_teeth = 'White'
-my_hair = 'Brown'
+name = 'Dover'
+age = 32 #in january 2017
+height = 180 #centimeters
+weight = 72 #kg
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+weight_pounds = weight * 2.20462
+height_inches = height / 2.8
 
-puts "Let's talk about #{my_name}."
-puts "He's #{my_height} inches tall."
-puts "He's #{my_weight} pounds heavy."
+
+puts "Let's talk about #{name}."
+puts "He's #{height_inches.round(2)} inches tall."
+puts "He's #{weight_pounds.round(2)} pounds heavy."
 puts "Actually that's not too heavy."
-puts "He's got #{my_eyes} eyes and #{my_hair}hair."
-puts "His teeth are usually #{my_teeth} depending on the coffee."
+puts "He's got #{eyes} eyes and #{hair}hair."
+puts "His teeth are usually #{teeth} depending on the coffee."
 
 
 # this line is tricky, try to get it exactly right
-puts "If I add my age #{my_age} , my height #{my_height}, and my weight #{my_weight} and I get #{my_age + my_height + my_weight}."
+puts "If I add my age #{age} , my height #{height}, and my weight #{weight} and I get #{age + height + weight}."
