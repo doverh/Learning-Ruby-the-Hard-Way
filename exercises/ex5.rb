6cdbc7a8103e31961b69c6163ce94ae14890b680
doverh <doverheld@gmail.com>
2017-01-17 10:33:37 -0500

    Adding exercise 5

---
+++ exercises/ex5.rb
@@ -0,0 +1,18 @@
+my_name = 'Dover'
+my_age = 32 #in january 2017
+my_height = 180 #centimeters
+my_weight = 72 #kg
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+puts "Let's talk about #{my_name}."
+puts "He's #{my_height} inches tall."
+puts "He's #{my_weight} pounds heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{my_eyes} eyes and #{my_hair}hair."
+puts "His teeth are usually #{my_teeth} depending on the coffee."
+
+
+# this line is tricky, try to get it exactly right
+puts "If I add my age #{my_age} , my height #{my_height}, and my weight #{my_weight} and I get #{my_age + my_height + my_weight}."
