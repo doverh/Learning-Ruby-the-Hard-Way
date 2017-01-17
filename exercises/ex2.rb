ce0168741841ee54736b12466174ecfd8ebf568f
doverh <doverheld@gmail.com>
2017-01-17 10:03:56 -0500

    adding exerc 4

--- exercises/ex2.rb
+++ exercises/ex2.rb
@@ -3,7 +3,7 @@
 
 puts "I could have code like this." # and the comment after is ignored
 
-You can also use a comment to "disable" or comment
+# You can also use a comment to "disable" or comment
 # puts "This won't run."
 
 puts "This will run"
