3781b83ccbd40ae9f8f5cfa3a58ee431700c9f83
doverh <doverheld@gmail.com>
2017-01-30 14:13:12 -0500

    Push of ex16

---
+++ exercises/ex16.rb
@@ -0,0 +1,35 @@
+filename = ARGV.first
+
+puts "We are going to erase #{filename}."
+puts "If you dont want that, hit CTRL-C(^C)."
+puts "If you do want, hit RETURN."
+
+$stdin.gets
+
+puts "Opening the file ..."
+target = open(filename, 'w')
+
+puts "Truncating the file. Goodbye"
+target.truncate(0)
+puts "Now I'm going to ask you for three lines."
+
+print "Line 1: "
+line1 = $stdin.gets.chomp
+
+print "Line 2: "
+line2 = $stdin.gets.chomp
+
+print "Line 3: "
+line3 = $stdin.gets.chomp
+
+puts "I'm going to write these to the file."
+
+target.write(line1)
+target.write("\n")
+target.write(line2)
+target.write("\n")
+target.write(line3)
+
+puts "And finaly we close it"
+
+target.close
